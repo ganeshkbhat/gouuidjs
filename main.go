475d7173f91f52fa81a1787f90d2a9aff7d8690b
Ganesh B <ganeshsurfs@gmail.com>
2024-02-19 23:18:10 +0530

    [COMMIT] Minor changes to codebase to add wasm files for gouuid

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
+/**
+ * 
+ * Package: gouuidjs
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i gouuidjs --save
+ * Github: https://github.com/ganeshkbhat/gouuidjs
+ * npmjs Link: https://www.npmjs.com/package/gouuidjs
+ * File: 
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
 package main
 
 import (
@@ -8,27 +23,25 @@ import (
 )
 
 func GetUUID(args []string) string {
-	total_args := len(args[1:])
+	total_args := len(os.Args[1:])
+	v := "."
 	if total_args > 0 {
-		v := strings.ToLower(args[1])
-		switch v {
-		case "v7":
-			id, err := uuid.NewV7()
-			if err != nil {
-				panic(err)
-			}
-			return id.String()
-		case "v6":
-			id, err := uuid.NewV6()
-			if err != nil {
-				panic(err)
-			}
-			return id.String()
-		default:
-			id := uuid.New()
-			return id.String()
+		v = strings.ToLower(args[1])
+	}
+	switch v {
+	case "v7":
+		id, err := uuid.NewV7()
+		if err != nil {
+			panic(err)
 		}
-	} else {
+		return id.String()
+	case "v6":
+		id, err := uuid.NewV6()
+		if err != nil {
+			panic(err)
+		}
+		return id.String()
+	default:
 		id := uuid.New()
 		return id.String()
 	}
