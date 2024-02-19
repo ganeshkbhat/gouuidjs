cc42aa4528070a1f54e2bfcc926a19e805e3eb4f
Ganesh B <ganeshsurfs@gmail.com>
2024-02-19 23:25:26 +0530

    [COMMIT] Adding test file structure for gouuidjs

--- build.wasm.bat
+++ build.wasm.bat
@@ -1,3 +1,7 @@
 go build
-$Env:GOOS = "js"; $Env:GOARCH = "wasm"; go build -o gouuid.wasm main.go
+set GOOS = "js"
+set GOARCH = "wasm"
+
+@REM $Env:GOOS = "js"; $Env:GOARCH = "wasm"; go build -o gouuid.wasm main.go
+cmd
 wasm-opt gouuid.wasm --enable-bulk-memory -Oz -o gouuid-bmem.wasm
