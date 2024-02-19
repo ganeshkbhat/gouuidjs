349912acea973e9a78fbe737e699d647889792a6
Ganesh B <ganeshsurfs@gmail.com>
2024-02-19 23:50:26 +0530

    [COMMIT] Adding test file structure for gouuidjs

--- demos/demos.wasm.mjs
+++ demos/demos.wasm.mjs
@@ -1,2 +1,21 @@
+/**
+ * 
+ * Package: gouuidjs
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i gouuidjs --save
+ * Github: https://github.com/ganeshkbhat/gouuidjs
+ * npmjs Link: https://www.npmjs.com/package/gouuidjs
+ * File: 
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
 import { GetUUID } from '../gouuid.wasm';
+import * as go from '../gouuid.wasm';
+
 console.log(GetUUID([".", "."]))
