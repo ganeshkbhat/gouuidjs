8242063acbaaa6531ab2de04de0039ee6dfd5c4e
Ganesh B <ganeshsurfs@gmail.com>
2024-02-20 14:25:41 +0530

    [COMMIT] Adding minor changes to script of build of wasm

--- demos/demos.load.gouuid.wasm.js
+++ demos/demos.load.gouuid.wasm.js
@@ -16,20 +16,18 @@
 'use strict';
 
 const fs = require('node:fs');
+const { GO } = require("./wasm.exec.js");
 const wasmBuffer = fs.readFileSync('./gouuid.wasm');
-const gojs = require("gojs");
+const { fetch } = import("node-fetch");
 
-const importObject = {
-  imports: {
-    imported_func(arg) {
-      console.log(arg);
-    },
-  },
-};
-
-WebAssembly.instantiate(wasmBuffer, importObject).then(wasmModule => {
-  const go = wasmModule.instance.exports;
-  console.log(go);
-  const uuid = go.GetUUID(["", "V7"]);
-  console.log(uuid); 
+const go = new Go();
+WebAssembly.instantiateStreaming(fetch("./gouuid.wasm"), go.importObject).then((result) => {
+  go.run(result.instance);
 });
+
+// WebAssembly.instantiate(wasmBuffer, importObject).then(wasmModule => {
+//   const go = wasmModule.instance.exports;
+//   console.log(go);
+//   const uuid = go.GetUUID(["", "V7"]);
+//   console.log(uuid);
+// });
